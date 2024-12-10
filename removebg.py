db1d0f0f5783681183a8453640c7d7042062f25d
Pramudya <pramudyap333@gmail.com>
2024-12-10 22:16:59 +0700

    update test.py add confusion matrix

--- removebg.py
+++ removebg.py
@@ -1,36 +1,10 @@
-# Install rembg
-# pip install rembg
-
-# from rembg import remove
-# from PIL import Image
-# import io
-
-# # Load image
-# input_path = 'dataset/training/normal/Healthy-Potato-Aug-30-_jpg.rf.ce884b0d345b1faa83ad31a1b1060005.jpg'  # Ganti dengan path gambar yang ingin kamu hapus background-nya
-# output_path = 'output_image.png'
-
-# # Buka gambar dan hapus background
-# with open(input_path, 'rb') as input_file:
-#     input_image = input_file.read()
-#     output_image = remove(input_image)
-
-# # Simpan gambar yang sudah dihapus background-nya
-# with open(output_path, 'wb') as output_file:
-#     output_file.write(output_image)
-
-# print("Background removed and saved to:", output_path)
-
-# Install libraries jika belum terpasang
-# pip install rembg tqdm pillow
-
 import os
 from rembg import remove
-from PIL import Image
 from tqdm import tqdm
 
 # Directory paths
-input_dir = 'dataset/testing/defective/proses'     # Ganti dengan direktori gambar yang ingin dihapus background-nya
-output_dir = 'dataset/testing/defective'    # Ganti dengan direktori tempat menyimpan hasil gambar
+input_dir = 'D:\semester 5\Final Project\\testcamera'     # Ganti dengan direktori gambar yang ingin dihapus background-nya
+output_dir = 'D:\semester 5\Final Project\\testcamera'    # Ganti dengan direktori tempat menyimpan hasil gambar
 
 # Buat output directory jika belum ada
 os.makedirs(output_dir, exist_ok=True)
--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import joblib
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load model and scaler
 knn = joblib.load("model/knn_model.pkl")
@@ -18,5 +20,13 @@ X_test = scaler.transform(X_test)
 y_pred = knn.predict(X_test)
 report = classification_report(y_test, y_pred, target_names=["normal", "defective"])
 
-# Print the results
+# Print the classification report
 print(report)
+
+# Generate and display the confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=["normal", "defective"], yticklabels=["normal", "defective"])
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.show()
